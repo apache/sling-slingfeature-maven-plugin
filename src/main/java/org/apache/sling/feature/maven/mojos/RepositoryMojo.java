ece43bb9fcdfb7822f1e2bf1d5473f9753d52cd2
Carsten Ziegeler <cziegeler@apache.org>
2018-10-01 11:47:51 +0200

    SLING-7962 : Add mojo for downloading all artifacts and putting them into a build directory. Fix includes/excludes

--- src/main/java/org/apache/sling/feature/maven/mojos/RepositoryMojo.java
+++ src/main/java/org/apache/sling/feature/maven/mojos/RepositoryMojo.java
@@ -31,6 +31,7 @@ import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
@@ -46,6 +47,7 @@ import org.apache.sling.feature.maven.ProjectHelper;
  */
 @Mojo(
         name = "repository",
+        defaultPhase = LifecyclePhase.PACKAGE,
         requiresDependencyResolution = ResolutionScope.TEST,
         threadSafe = true
     )
