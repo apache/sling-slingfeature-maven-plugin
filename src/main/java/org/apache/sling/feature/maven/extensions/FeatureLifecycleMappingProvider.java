15be440adfc046f859a68203a74eace080a1740e
Konrad Windszus <kwin@apache.org>
2023-07-12 19:50:27 +0200

    SLING-11947 Convert Plexus to JSR 330 components

---
+++ src/main/java/org/apache/sling/feature/maven/extensions/FeatureLifecycleMappingProvider.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.feature.maven.extensions;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Named;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+import org.apache.maven.lifecycle.mapping.Lifecycle;
+import org.apache.maven.lifecycle.mapping.LifecycleMapping;
+import org.apache.maven.lifecycle.mapping.LifecyclePhase;
+
+@Singleton
+@Named("slingosgifeature")
+public class FeatureLifecycleMappingProvider implements Provider<LifecycleMapping> {
+
+    private static final String DEFAULT_LIFECYCLE_KEY = "default";
+    private static final Map<String, LifecyclePhase> BINDINGS;
+    static {
+        BINDINGS = new HashMap<>();
+        BINDINGS.put("install", new LifecyclePhase("org.apache.sling:slingfeature-maven-plugin:attach-features,org.apache.maven.plugins:maven-install-plugin:install"));
+        BINDINGS.put("deploy", new LifecyclePhase("org.apache.maven.plugins:maven-deploy-plugin:deploy"));
+    }
+
+    private final Lifecycle defaultLifecycle;
+
+    private final LifecycleMapping lifecycleMapping;
+
+    public FeatureLifecycleMappingProvider() {
+        this.defaultLifecycle = new Lifecycle();
+        this.defaultLifecycle.setId(DEFAULT_LIFECYCLE_KEY);
+        this.defaultLifecycle.setLifecyclePhases(BINDINGS);
+
+        this.lifecycleMapping = new LifecycleMapping() {
+            @Override
+            public Map<String, Lifecycle> getLifecycles() {
+                return Collections.singletonMap(DEFAULT_LIFECYCLE_KEY, defaultLifecycle);
+            }
+
+            @Override
+            public List<String> getOptionalMojos(String lifecycle) {
+                return null;
+            }
+
+            @Override
+            public Map<String, String> getPhases(String lifecycle) {
+                if (DEFAULT_LIFECYCLE_KEY.equals(lifecycle)) {
+                    return defaultLifecycle.getPhases();
+                } else {
+                    return null;
+                }
+            }
+        };
+    }
+
+    @Override
+    public LifecycleMapping get() {
+        return lifecycleMapping;
+    }
+
+}
--- src/site/markdown/index.md
+++ src/site/markdown/index.md
@@ -5,5 +5,27 @@ Maven Plugin for building OSGi features and applications.
 
 See [Goals](plugin-info.html) for a list of supported goals.
 
-### Usage
+## Usage
+
+As this Maven plugin comes with [Maven extensions](https://maven.apache.org/guides/mini/guide-using-extensions.html) (for defining custom bindings for `default` lifecycle and a custom artifact handler for type/packaging `slingosgifeature` as well as a `AbstractMavenLifecycleParticipant`) it needs to be loaded accordingly
+
+```
+<plugin>
+  <groupId>${project.groupId}</groupId>
+  <artifactId>${project.artifactId}</artifactId>
+  <version>${project.version}</version>
+  <extensions>true</extensions>
+</plugin>
+```
+
 Additional Documentation can be found in the [README](https://github.com/apache/sling-slingfeature-maven-plugin/blob/master/README.md) page.
+
+## Plugin bindings for `slingosgifeature` packaging
+
+```
+<phases>
+  <install>org.apache.sling:slingfeature-maven-plugin:attach-features,
+           org.apache.maven.plugins:maven-install-plugin:install</install>
+  <deploy>org.apache.maven.plugins:maven-deploy-plugin:deploy</deploy>
+</phases>
+```
